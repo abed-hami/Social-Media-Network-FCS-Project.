9251213d945af962a62ce40bc3f38fdf691eb1ab
abedhamiyeh <abedhamye28@gmail.com>
2024-07-20 14:59:57 +0300

    adding and removing user methods

--- graph.py
+++ graph.py
@@ -1,3 +1,22 @@
 class Graph:
     def __init__(self):
         self.graph = {}
+    
+    def add_user(self,user):
+        #check for existance of the user in the graph
+        if user not in self.graph:
+            #adds the user as a key and the value is an empty list to add friends in
+            self.graph[user]=[]
+    
+    def remove_user(self,user):
+        #check for existance of the user in the graph
+        if user in self.graph:
+            #deletes the user from the graph
+            del self.graph[user]
+            #loops over the values aka friens list
+            for friends in self.graph.values():
+                #if the user is one of the friends it is removed
+                if user in friends:
+                    friends.remove(user)
+
+    
