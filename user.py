1e36c4c4443ebb422dcb5833787eb0e299077aae
abedhamiyeh <abedhamye28@gmail.com>
2024-07-20 15:17:31 +0300

    update user profile

--- user.py
+++ user.py
@@ -1,7 +1,8 @@
 class User:
-    def __init__(self,id,name):
+    def __init__(self,id,name,email):
         self.id = id
         self.name = name
+        self.email = email
         self.friends=[]
         self.interests=[]
         self.posts=[]
@@ -11,3 +12,9 @@ class User:
 
     def add_interest(self,interest):
         self.posts.append(interest)
+
+    def update_profile(self,name=None,email=None):
+        if name:
+            self.name = name
+        if email:
+            self.email=email
