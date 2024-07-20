a5618dbf2ee45ec0d378c79e0d307bd94f028442
abedhamiyeh <abedhamye28@gmail.com>
2024-07-20 15:15:01 +0300

    add post/ interest methods

--- user.py
+++ user.py
@@ -3,5 +3,11 @@ class User:
         self.id = id
         self.name = name
         self.friends=[]
+        self.interests=[]
+        self.posts=[]
         
-        
+    def add_post(self,post):
+        self.posts.append(post)
+
+    def add_interest(self,interest):
+        self.posts.append(interest)
